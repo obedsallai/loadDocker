6153fb491088245cc7f511069b098c606f9ce163
obedkawa <obed@fluidpay.link>
2026-03-20 20:10:58 +0100

    modif du fichier config.js

--- config/config.js
+++ config/config.js
@@ -1,17 +1,9 @@
-const {Pool} = require('pg');
-require('dotenv').config();
- try
-  {
-  const pool = new Pool({
-      user:process.env.USERNAME,
-      host:process.env.HOSTNAME,
-      database:process.env.DATABASE,
-      password:process.env.PASSWORD,
-      port:process.env.DATABASE_PORT
-    })
-  }catch(err)
-  {
-    console.error('Error connecting to PostgreSQL database', err);
+module.exports = {
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'postgres'
   }
-console.log('Connected to PostgreSQL database');
-module.export = pool;
+};
