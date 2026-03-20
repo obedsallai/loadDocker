94b6e822333ebfe72bd9d0a8389a2f7634e37876
obedkawa <obed@fluidpay.link>
2026-03-20 19:40:03 +0100

    modif de configuration pour la base de donnees

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+require('config/config');
 
 const { Sequelize } = require('sequelize');
 const sequelize = require('./models').sequelize;
@@ -9,6 +10,7 @@ const Todo = require('./models').Todo;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 // Routes
 app.get('/todos', async (req, res) => {
--- config/config.js
+++ config/config.js
@@ -1,18 +1,17 @@
+const {Pool} = require('pg');
 require('dotenv').config();
-
-module.exports = {
-  development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-  },
-  production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-};
+ try
+  {
+  const pool = new Pool({
+      user:process.env.USERNAME,
+      host:process.env.HOSTNAME,
+      database:process.env.DATABASE,
+      password:process.env.PASSWORD,
+      port:process.env.DATABASE_PORT
+    })
+  }catch(err)
+  {
+    console.error('Error connecting to PostgreSQL database', err);
+  }
+console.log('Connected to PostgreSQL database');
+module.export = pool;
